4a0dfee1a569f8311089f2458fe9f557e69642ef
CodingManoj <mvp2612@hotmail.com>
2023-04-24 23:03:25 -0400

    Git Auto Push

--- docdb.tf
+++ docdb.tf
@@ -7,6 +7,7 @@ resource "aws_docdb_cluster" "docdb" {
   # preferred_backup_window = "07:00-09:00"
   skip_final_snapshot     = true
   db_subnet_group_name    = aws_docdb_subnet_group.docdb_subnet_group.name
+  vpc_security_group_ids  = [aws_security_group.allow_mongodb.id]
 }
 
 
