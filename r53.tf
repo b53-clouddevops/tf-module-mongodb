b2c37d7bbcc19ac92867810642b50d212163d40b
CodingManoj <mvp2612@hotmail.com>
2023-04-25 23:34:55 -0400

    Git Auto Push

--- r53.tf
+++ r53.tf
@@ -1,8 +1,8 @@
 # This created the CNAME Record for MongoDB
-resource "aws_route53_record" "mongodb" {
-  zone_id = "Z096213084GYRYZXR9VK"
-  name    = "www.example.com"
-  type    = "A"
-  ttl     = 300
-  records = [aws_eip.lb.public_ip]
-}
+# resource "aws_route53_record" "mongodb" {
+#   zone_id = "Z096213084GYRYZXR9VK"
+#   name    = "www.example.com"
+#   type    = "A"
+#   ttl     = 300
+#   records = [aws_eip.lb.public_ip]
+# }
