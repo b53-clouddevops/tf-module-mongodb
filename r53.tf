ef8132e0a3159bfe69155387304e5fc9337f24e6
CodingManoj <mvp2612@hotmail.com>
2023-04-26 22:39:16 -0400

    Git Auto Push

--- r53.tf
+++ r53.tf
@@ -1,7 +1,7 @@
 # This created the CNAME Record for MongoDB
 resource "aws_route53_record" "mongodb" {
   zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
-  name    = "mongodb-${var.ENV}.data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME"
+  name    = "mongodb-${var.ENV}"
   type    = "CNAME"
   ttl     = 10
   records = [aws_docdb_cluster.docdb.endpoint]
