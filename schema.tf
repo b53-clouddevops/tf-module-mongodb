c5a03b12765e8c11201b918b05f8b09f143d2ac6
CodingManoj <mvp2612@hotmail.com>
2023-05-08 20:56:57 -0400

    Git Auto Push

--- schema.tf
+++ schema.tf
@@ -1,7 +1,7 @@
 # This is going to inject the MySQL Schema
 resource "null_resource" "schema" {
 
-    depends_on = [aws_docdb_cluster.docdb]
+    depends_on = [aws_docdb_cluster.docdb, aws_docdb_cluster_instance.cluster_instances]
 
   provisioner "local-exec" {
         command = <<EOF
